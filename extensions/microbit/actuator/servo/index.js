d179462f941028d6f9b867e248a552404f455986
aopu-edu <postmaster@mircoai.com>
2022-03-26 18:01:22 +0800

    add microbit extensions

---
+++ extensions/microbit/actuator/servo/index.js
@@ -0,0 +1,29 @@
+const servo = formatMessage => ({
+    name: formatMessage({
+        id: 'servo.name',
+        default: 'Servo',
+        description: 'Name of servo'
+    }),
+    extensionId: 'servo',
+    version: '1.0.0',
+    type: 'microbit',
+    supportDevice: ['microbit', 'microbitV2'],
+    author: 'ArthurZheng',
+    iconURL: `asset/servo.png`,
+    description: formatMessage({
+        id: 'servo.description',
+        default: 'Classic servo support for microbit.',
+        description: 'Description of servo'
+    }),
+    featured: true,
+    blocks: 'blocks.js',
+    generator: 'generator.js',
+    toolbox: 'toolbox.js',
+    msg: 'msg.js',
+    library: 'lib',
+    official: true,
+    tags: ['actuator'],
+    helpLink: 'https://openblockcc.gitee.io/wiki/main'
+});
+
+module.exports = servo;
---
+++ extensions/microbit/actuator/servo/msg.js
@@ -0,0 +1,35 @@
+// This file was automatically generated.  Do not modify.
+/* eslint-disable func-style */
+/* eslint-disable require-jsdoc */
+/* eslint-disable quote-props */
+/* eslint-disable quotes */
+/* eslint-disable dot-notation */
+
+function addMsg (Blockly) {
+
+    Object.assign(Blockly.ScratchMsgs.locales["en"],
+        {
+            "SERVO_CATEGORY": "Servo",
+            "SERVO_WRITE": "set servo pin %1 out %2"
+        }
+    );
+
+    Object.assign(Blockly.ScratchMsgs.locales["zh-cn"],
+        {
+            "SERVO_CATEGORY": "舵机",
+            "SERVO_WRITE": "设置引脚 %1 舵机为 %2"
+        }
+    );
+
+    Object.assign(Blockly.ScratchMsgs.locales["zh-tw"],
+        {
+            "SERVO_CATEGORY": "舵機",
+            "SERVO_WRITE": "設置管腳 %1 舵機為 %2"
+        }
+    );
+
+    return Blockly;
+}
+
+exports = addMsg;
+// End of combined translations
