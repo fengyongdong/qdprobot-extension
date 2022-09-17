29044d5ddd65aad536f42f70d721f0921aedd50c
feng <fydongmail@163.com>
2022-09-17 17:29:05 +0800

    更新RGB灯下拉表

--- extensions/arduino/kit/QDPRobotC02/blocks.js
+++ extensions/arduino/kit/QDPRobotC02/blocks.js
@@ -2277,7 +2277,7 @@ function addBlocks (Blockly) {
                     {
                         type: 'field_dropdown',
                         name: 'PIN1',
-                        options: qdprobotESP32_R_PIN_Output_Dropdown
+                        options: qdprobotESP32_L_PIN_Output_Dropdown
                     },
                     {
                         type: 'input_value',
@@ -2326,7 +2326,7 @@ function addBlocks (Blockly) {
                     {
                         type: 'field_dropdown',
                         name: 'PIN1',
-                        options: qdprobotESP32_R_PIN_Output_Dropdown
+                        options: qdprobotESP32_L_PIN_Output_Dropdown
                     },
                     {
                         type: 'input_value',
