c0a044d15016824ae839a4b60635e9d5c918b62f
feng <fydongmail@163.com>
2022-11-19 16:32:38 +0800

    更新MP3 for mpy

--- extensions/microPython/actuator/QHDFPlayer/generator.js
+++ extensions/microPython/actuator/QHDFPlayer/generator.js
@@ -7,49 +7,49 @@ function addGenerator (Blockly) {
       var tx = this.getFieldValue('tx');
       var rx = this.getFieldValue('rx');
       var uart = this.getFieldValue('uart');
-      Blockly.Python.imports_['import_DFPlay'] = 'import DFPlay';
-      var code=`play = DFPlay.SimpleDFPlayerMini(uart_id=${uart},tx=${tx},rx=${rx})\n`;
+      Blockly.Python.imports_['import_QDPPlay'] = 'import QDPPlay';
+      var code=`play = QDPPlay.SimpleQDPPlayerMini(uart_id=${uart},tx=${tx},rx=${rx})\n`;
       return code;  
     };
 
     Blockly.Python.mpy_esp32_Mp3_vol_change = function () {
       var vol = this.getFieldValue('vol');
-      Blockly.Python.imports_['import_DFPlay'] = 'import DFPlay';
+      Blockly.Python.imports_['import_QDPPlay'] = 'import QDPPlay';
       var code=`play.${vol}()\n`;
       return code;  
     };
 
     Blockly.Python.mpy_esp32_Mp3_vol_set = function () {
       var vol = Blockly.Python.valueToCode(this, 'vol',Blockly.Python.ORDER_ATOMIC) ||'0' ;
-      Blockly.Python.imports_['import_DFPlay'] = 'import DFPlay';
+      Blockly.Python.imports_['import_QDPPlay'] = 'import QDPPlay';
       var code=`play.set_vol(${vol})\n`;
       return code;  
     };
 
     Blockly.Python.mpy_esp32_Mp3_set_EQ = function () {
       var mode = this.getFieldValue('mode');
-      Blockly.Python.imports_['import_DFPlay'] = 'import DFPlay';
+      Blockly.Python.imports_['import_QDPPlay'] = 'import QDPPlay';
       var code=`play.set_eq(${mode})\n`;
       return code;  
     };
 
     Blockly.Python.mpy_esp32_Mp3_prev_next_track = function () {
       var mode = this.getFieldValue('mode');
-      Blockly.Python.imports_['import_DFPlay'] = 'import DFPlay';
+      Blockly.Python.imports_['import_QDPPlay'] = 'import QDPPlay';
       var code=`play.${mode}()\n`;
       return code;  
     };
 
     Blockly.Python.mpy_esp32_Mp3_cycle_singles = function () {
       var num = Blockly.Python.valueToCode(this, 'num',Blockly.Python.ORDER_ATOMIC) ||'0' ;
-      Blockly.Python.imports_['import_DFPlay'] = 'import DFPlay';
+      Blockly.Python.imports_['import_QDPPlay'] = 'import QDPPlay';
       var code=`play.set_mode(${num})\n`;
       return code;  
     };
 
     Blockly.Python.mpy_esp32_Mp3_play_pause_stop = function () {
       var mode = this.getFieldValue('mode');
-      Blockly.Python.imports_['import_DFPlay'] = 'import DFPlay';
+      Blockly.Python.imports_['import_QDPPlay'] = 'import QDPPlay';
       var code=`play.${mode}()\n`;
       return code;  
     };
